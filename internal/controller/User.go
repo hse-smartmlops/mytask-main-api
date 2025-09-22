c699e8263933ed20ea55b70b9d0ad23c18bfb2a3
Александр Трусанов <200taa@mail.ru>
2025-09-22 17:56:10 +0300

    fix(#12): попытка фикса 502 номер 8

--- internal/controller/User.go
+++ internal/controller/User.go
@@ -272,7 +272,7 @@ func CreateUserWithIdFunc(req request.UserCreateRequest, c echo.Context, id uuid
 		TgID: tgId,
 		TgUserID: tgUserID,
 		Profession: profession,
-		UserRoles: nil,
+		UserRoles: []models.UserRole{},
 	}
 
 	// Логируем входные данные (без чувствительных полей)
